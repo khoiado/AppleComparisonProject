7471c0a486bc9002f44eb7bd8d5e2fc0a5fc18bb
Khoi Do <khoido.kad@gmail.com>
2023-08-29 17:40:22 -0500

    First Commit

---
+++ AppleDeviceComparisonTool/main.cpp
@@ -0,0 +1,105 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QTimer>
+#include <QMessageBox>
+
+void displaySpecifications(MainWindow *window);
+QString getSpecifications(const QString &device);
+
+//Created a function that connects button's clicked signal to displaySpecifications function
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    QObject::connect(w.ui->pushButton, &QPushButton::clicked, &w, [&w](){
+        w.ui->textEdit->clear(); //Clearing text boxes immediately when the "compare" button is pressed
+        w.ui->textEdit_2->clear(); //Clearing text boxes immediately when the "compare" button is pressed
+        displaySpecifications(&w);
+    });
+    return a.exec();
+}
+
+void displaySpecifications(MainWindow *window)
+{
+    window->ui->label_2->setText("Comparing...");
+    window->ui->label_2->setVisible(true);
+
+    QTimer::singleShot(1000, window, [window]()
+    {
+        QString device1 = window -> ui -> comboBox -> currentText();
+        QString device2 = window -> ui -> comboBox_2 -> currentText();
+
+        QString specs1 = getSpecifications(device1);
+        QString specs2 = getSpecifications(device2);
+
+        //Clearing the text boxes before the error message box appears
+
+        window->ui->textEdit->clear();
+        window->ui->textEdit_2->clear();
+
+        //Error message box if device specs not found
+
+        if (specs1 == "Specifications not found.") {
+            QMessageBox::critical(window, "Error", "Specifications not found for " + device1 + ".");
+        } else {
+            window->ui->textEdit->setText("Specifications for " + device1 + ":\n" + specs1);
+        }
+
+        if (specs2 == "Specifications not found.") {
+            QMessageBox::critical(window, "Error", "Specifications not found for " + device2 + ".");
+        } else {
+            window->ui->textEdit_2->setText("Specifications for " + device2 + ":\n" + specs2);
+        }
+
+        window -> ui -> textEdit -> setText("Specifications for " + device1 + ":\n" + specs1);
+        window -> ui -> textEdit_2 -> setText("Specifications for " + device2 + ":\n" + specs2);
+
+        window->ui->label_2->setVisible(false);
+    });
+}
+
+//Function that fetches specifications for the selected devices
+
+QString getSpecifications(const QString &device)
+{
+    QFile file("/home/khoi/Apple Project/device-specs-data");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        return "Error reading specifications.";
+    }
+
+    QTextStream in(&file);
+    QString line;
+    QString specs;
+    bool found = false;
+
+    while (!in.atEnd()) {
+        line = in.readLine();
+        if (line == device) {
+            found = true;
+            specs = line + "\n";
+            while (!in.atEnd()) {
+                line = in.readLine();
+                if (line.isEmpty()) {
+                    break;
+                }
+                specs += line + "\n";
+            }
+            break;
+        }
+    }
+
+    file.close();
+
+    if (found) {
+        return specs;
+    } else {
+        return "Specifications not found.";
+    }
+}
+
+
